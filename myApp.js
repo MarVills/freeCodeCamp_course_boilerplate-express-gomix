c18f3e4d6dfc344cc1f3ca7a4afb77702ae25b2e
Marben Villaflor <100498636+MarVills@users.noreply.github.com>
2022-06-02 15:13:29 +0000

    last commit for the day

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+absolutePath =  __dirname + "/views/index.html";
+
+
+app.get("/", function(express, app) {
+  app.sendFile(absolutePath);
+});
 
 
 
