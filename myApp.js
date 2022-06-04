e71355e5b4a2566dee23c67f024fb91dda608652
Marben Villaflor <100498636+MarVills@users.noreply.github.com>
2022-06-04 11:29:45 +0000

    push latest progress

--- myApp.js
+++ myApp.js
@@ -2,18 +2,27 @@ let express = require('express');
 let app = express();
 // console.log("Hello World");
 
-absolutePath =  __dirname + "/views/index.html";
+// absolutePath =  __dirname + "/views/index.html";
 
 
 // app.get("/", function(express, app) {
 //   app.sendFile(absolutePath);
 // });
-// app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", (express, app) => {
-  app.json({
-    message: "Hello json"
-  });
+app.get("/json", function(express, res) {
+ 
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+  // response = "Hello World".toUpperCase();
+     res.json(
+       {"message": "HELLO JSON"}
+  )
+} else {
+  // response = "Hello World";
+      res.json(
+        {"message": "Hello json"}
+  )
+}
 });
 
 
