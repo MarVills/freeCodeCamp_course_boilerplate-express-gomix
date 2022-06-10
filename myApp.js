181e03d398a539b5747311745313397b19883e58
Marben Villaflor <100498636+MarVills@users.noreply.github.com>
2022-06-10 04:34:40 +0000

    Get Route Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -43,11 +43,22 @@ app.get("/now", middleware, (req, res) => {
 });
 
 app.get("/:word/echo", (req, res) => {
-  const { word } = req.params;
+  // const { word } = req.params;
   res.json({
-    echo: word
+    echo: req.params.word
   });
 });
+
+// app.get("/name", function(req, res) {
+//   // var firstName = req.query.first;
+//   // var lastName = req.query.last;
+//   // OR you can destructure and rename the keys
+//   // var { first: firstName, last: lastName } = req.query;
+//   // Use template literals to form a formatted string
+//   res.json({
+//     name: req.query.first + " " + req.query.last
+//   });
+// });
 
 
 
